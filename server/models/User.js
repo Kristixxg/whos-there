203ebf33a1a920ec4e1f47e7c65f493d0724de15
Keerti Chaudhary <17keerti@gmail.com>
2022-06-04 16:00:34 -0700

    hash user password using bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -25,6 +26,16 @@ const userSchema = new Schema(
     },
   }
 );
+
+// hash user password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
 
 const User = model("User", userSchema);
 
