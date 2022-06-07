245434f123d4c08cb2630f109aa5d99939e24f6a
CT <tellezc007@gmail.com>
2022-06-07 14:12:25 -0700

    import  and create Switch Routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import {ApolloClient} from '@apollo/client';
+import {RoBrowserRouter as Router, Switch, 
+  Route, Redirect,uter} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloClient>
+    <Router>
+      <div>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/homepage' component={Home} />
+        <Route exact path='/profile' component={Profile} />
+      </Switch>
+      </div>
+    </Router>
+    </ApolloClient>
   );
 }
 
 export default App;
+
