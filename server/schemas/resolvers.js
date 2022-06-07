2813d008bd432521c157d3ba7c9d02e9b1d69bf3
Keerti Chaudhary <17keerti@gmail.com>
2022-06-06 18:23:07 -0700

    add mutation for adding user and login in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,31 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
   },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
