57ccf2e336d498b707aded7fcd88beabc72e5838
CT <tellezc007@gmail.com>
2022-06-07 19:24:45 -0700

    import and create the routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,25 @@ import Login from './pages/Login';
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
-import {ApolloClient} from '@apollo/client';
-import {RoBrowserRouter as Router, Switch, 
-  Route, Redirect,uter} from 'react-router-dom';
-
+// import {ApolloClient} from '@apollo/client';
+import { BrowserRouter as Router, Routes, 
+  Route} from 'react-router-dom';
+  import { ClientOnly } from "react-client-only";
 
 function App() {
   return (
-    <ApolloClient>
+    // <ApolloClient>
+    <ClientOnly>
     <Router>
-      <div>
-      <Switch>
-        <Route exact path='/' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/homepage' component={Home} />
-        <Route exact path='/profile' component={Profile} />
-      </Switch>
-      </div>
+      <Routes>
+        <Route  path='/' element={<Login/>} />
+        <Route  path='/signup' element={<Signup/>} />
+        <Route  path='/homepage' element={<Home/>} />
+        <Route  path='/profile' element={<Profile/>} />
+      </Routes>
     </Router>
-    </ApolloClient>
+    </ClientOnly>
+  //  </ApolloClient>
   );
 }
 
