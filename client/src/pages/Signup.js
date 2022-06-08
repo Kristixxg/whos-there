ca990d840ddf3bb691d942674c09d7ab5b2f9e44
CT <tellezc007@gmail.com>
2022-06-07 19:23:19 -0700

    add home, login, and create boilerplate for profile and home

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function Signup(props) {
+function Signup() {
 
-    
+    return(
 
-    return;
+        <>
+<div className="container">
+    <form className="form-area signup-form">
+        <div className="form-text">
+            <h1>Signup Form</h1>
+            <div className="input-one"><input type="text" placeholder="Username" id="name-signup"/></div>
+            <div className="input-one"><input type="email" placeholder="Email" id="email-signup"/>
+            </div>
+            <div className="input-one"><input type="password" placeholder="Password" id="password-signup"/>
+            </div>
+            <button className="signupbtn" type="submit">Sign Up</button>
+        </div>
+    </form>
+</div>
+
+        </>
+    )
 
 }
 
