fbfefaef5464dfdc1ee6b4b124430255c6f01d53
Kristy Guo <krisxxg@gmail.com>
2022-06-08 17:03:28 -0700

    import react-google-maps-api and use-places-autocomplete api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
+
+import React, { useState } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+
+import {formatRelative} from 'date-fns';
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+
+
+
+
+
+
+
+
 
 function Home() {
 
