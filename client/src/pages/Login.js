0286557adae34d24c55782f47a9158a8ba5f6f32
CT <tellezc007@gmail.com>
2022-06-10 10:41:55 -0700

    add values and try to debug file

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,67 +1,87 @@
 import React, { useState } from "react";
 import { useMutation } from '@apollo/client';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import Auth from '../utils/auth';
 import { LOGIN_USER } from '../utils/mutations'
+// import { useNavigate } from "react-router-dom";
 
-
-function Login(props) {
+const Login = () => {
 
     const [formState, setFormState] = useState({ username: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN_USER);
-
-    const handleFormSubmit = async (event) => {
-        event.prevenDefault();
-        try {
-            const mutationResponce = await login({
-                variables: { username: formState.username, password: formState.password },
-            });
-            const token = mutationResponce.data.login.token;
-            Auth.login(token);
-        } catch (e) {
-            console.log(e);
-        }
-    };
+    const [login, { error, data }] = useMutation(LOGIN_USER);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormState({
-          ...formState,
-          [name]: value,
+            ...formState,
+            [name]: value,
         });
-      };
+    };
 
 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        console.log('hello')
+        try {
+            console.log('im over hereeeeeee!!!')
+            const { data } = await login({
+                variables: { ...formState },
+            });
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+        }
+
+           setFormState({
+            username: '',
+            password: '',
+          });
+
+        //   const nav = useNavigate();
+    }
     return (
         <>
             <div className="container">
                 <div className="form-area">
-                    <form className='login-form' onSubmit={handleFormSubmit}>
-                        <div className="form-text">
-                            <h1>Login</h1>
-                            <div className="input"><input type='text' placeholder="Username" id="usernameLogin" onChange={handleChange}></input></div>
-                            <div className="input"><input type='password' placeholder="Password" id="passwordLogin" onChange={handleChange}></input></div>
-                            <Link to='/homepage'>
-                            <button className="loginBtn" type="submit">Login</button>
-                            </Link>
-                        </div>
-                        {error ? (
-                            <div>
-                                <p className="error-text">The provided credentials are incorrect</p>
+                    {data ? (
+                            <p>
+                            Success! You may now head{' '}
+                            <Link to="/homepage">back to the homepage.</Link>
+                            {/* <Navigate to="/signup"/>; */}
+                          </p>
+                    ) : (
+                        <form className='login-form' onSubmit={handleFormSubmit}>
+                            <div className="form-text">
+                                <h1>Login</h1>
+                                <div className="input"><input type='text' placeholder="Username" value={formState.username} name="username" onChange={handleChange}></input></div>
+                                <div className="input"><input type='password' placeholder="Password" value={formState.password} name="password" onChange={handleChange}></input></div>
+                                <button className="loginBtn" type="submit">Login</button>
                             </div>
-                        ) : null}
-                        <div className="signupBtn">
-                            <h3>Already Have An Account?</h3>
-                            <Link to='/signup'>
-                                <button className="aBtn" type="submit">Sign Up!</button>
-                            </Link>
+                            {error ? (
+                                <div>
+                                    <p className="error-text">The provided credentials are incorrect</p>
+                                </div>
+                            ) : null}
+                            <div className="signupBtn">
+                                <h3>Already Have An Account?</h3>
+                                <Link to='/signup'>
+                                    <button className="aBtn" type="submit">Sign Up!</button>
+                                </Link>
+                            </div>
+                        </form>
+                    )}
+                    {error && (
+                        <div className="my-3 p-3 bg-danger text-white">
+                            {error.message}
                         </div>
-                    </form>
+                    )}
                 </div>
             </div>
 
         </>
     );
 }
+
 
 export default Login;
