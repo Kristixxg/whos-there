3e05a0d7043fea310e49ba409f03fb4b08a5bd55
Keerti Chaudhary <17keerti@gmail.com>
2022-06-10 11:27:33 -0700

    add query to get all users and user by id on client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,3 +17,23 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      email
+    }
+  }
+`;
