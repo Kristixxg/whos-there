c7d60ff43add61db4ec9fadc3fc7935614e90f88
Kristy Guo <krisxxg@gmail.com>
2022-06-09 20:14:32 -0700

    add nav react component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,1 +1,20 @@
 import React from "react";
+import './nav.css';
+
+
+
+function Nav() {
+    return (
+        <nav>
+            <h1 className='titleh1'>WHOS THERE <img className="titleImage" src="./images/003-tennis-ball-1.png"></img></h1>
+            <ul>
+                <li className="button-68" ><a href='/homepage'>HOME</a></li>
+                <li className="button-68" ><a href='/profile'>PROFILE</a></li>
+                <li className="button-68" ><a href='/'>LOGIN</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
+
+
