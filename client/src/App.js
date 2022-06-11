acea15095dd4e2d9d031e262f9f171fa392d7d35
Keerti Chaudhary <17keerti@gmail.com>
2022-06-11 00:31:50 -0700

    change route of homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './App.css';
-import Login from './pages/Login';
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Signup from './pages/Signup'
-import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { setContext } from '@apollo/client/link/context';
+import React from "react";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -31,21 +35,16 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
-
-
 function App() {
   return (
     <ApolloProvider client={client}>
-      
       <Router>
         <Nav />
         <Routes>
-
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/homepage' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/' element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
       <Footer />
@@ -54,4 +53,3 @@ function App() {
 }
 
 export default App;
-
